7b6de91048ff2b70d1db8c69c3a2ecb1fee99158
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-01 13:42:14 +0100

    Do not try lock in an infinite loop

--- src/main/java/dk/statsbiblioteket/metadatarepository/xmltapes/common/LockPoolImpl.java
+++ src/main/java/dk/statsbiblioteket/metadatarepository/xmltapes/common/LockPoolImpl.java
@@ -1,8 +1,5 @@
 package dk.statsbiblioteket.metadatarepository.xmltapes.common;
 
-import dk.statsbiblioteket.metadatarepository.xmltapes.common.LockPool;
-
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -12,7 +9,8 @@ import java.util.concurrent.locks.ReentrantLock;
  * Time: 12:42 PM
  * To change this template use File | Settings | File Templates.
  */
-public class LockPoolImpl extends LockPool {
+public class LockPoolImpl
+        extends LockPool {
 
     private ReentrantLock lock;
 
@@ -23,16 +21,7 @@ public class LockPoolImpl extends LockPool {
 
     @Override
     public void lockForWriting() {
-        while (true){
-            try {
-                if (lock.tryLock(10, TimeUnit.MILLISECONDS)){
-                    break;
-                }
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-        }
+        lock.lock();
     }
 
     @Override
