fb0cd0ad8ba82c00ac95a8d67dc32ac9d5f502b7
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-06-19 12:22:30 +0200

    Added a rebuild test

--- src/test/java/dk/statsbiblioteket/metadatarepository/xmltapes/redis/RedisIndexTest.java
+++ src/test/java/dk/statsbiblioteket/metadatarepository/xmltapes/redis/RedisIndexTest.java
@@ -2,14 +2,18 @@ package dk.statsbiblioteket.metadatarepository.xmltapes.redis;
 
 import dk.statsbiblioteket.metadatarepository.xmltapes.common.index.Entry;
 import dk.statsbiblioteket.metadatarepository.xmltapes.common.index.Record;
+import dk.statsbiblioteket.metadatarepository.xmltapes.tarfiles.TapeArchiveImpl;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.UUID;
@@ -41,6 +45,26 @@ public class RedisIndexTest {
         index.clear();
     }
 
+    @Test
+    public void testRebuild() throws IOException, URISyntaxException {
+        long tapeSize = 1024L * 1024;
+        TapeArchiveImpl tapeArchive = new TapeArchiveImpl(getPrivateStoreId(), tapeSize, ".tar", "tape", "tempTape");
+        tapeArchive.setIndex(index);
+        index.clear();
+        //Test that the index is clear
+        tapeArchive.setRebuild(true);
+        tapeArchive.init();
+        Assert.assertTrue(tapeArchive.exist(URI.create("info:fedora/uuid:0cab5f49-b6ed-47d6-87d8-a257ba782246")));
+        Assert.assertFalse(tapeArchive.exist(URI.create("uuid:0cab5f49-b6ed-47d6-87d8-a257ba782246")));
+    }
+
+    private static File getPrivateStoreId() throws URISyntaxException {
+        File archiveFolder = new File(Thread.currentThread()
+                                            .getContextClassLoader()
+                                            .getResource("reindexTest/tape.tar")
+                                            .toURI()).getParentFile();
+        return archiveFolder;
+    }
     @Ignore
     @Test
     public void testIterate() throws Exception {
@@ -124,4 +148,6 @@ public class RedisIndexTest {
     private Entry getEntry(URI uuid){
         return new Entry(new File("/"),0);
     }
+
+
 }
